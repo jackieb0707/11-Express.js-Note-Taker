9064c3c35e26bd3da5eb095c89c20ea7c61368da
jackieb0707 <jackiebell446@yahoo.com>
2023-01-06 08:19:24 -0600

    develop

---
+++ Develop/db/DB.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const util = require('util');
+
+const noteData = './db/db.json';
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class  DB {
+  async readNotes(){
+    try {
+        const notesRaw = await readFileAsync(noteData, 'UTF8');
+        return notesRaw ? JSON.parse(notesRaw): [];
+
+    } catch (error){
+        throw error;
+    }
+  }
+
+ async getAndRenderNotes(data) {
+    try {
+        await writeFileAsync(noteData, JSON.stringify(data, null, '\t')). then (() =>{
+            console.log('New note added.');
+        }
+        );
+    } catch (error) {
+        throw error;
+    }
+ }
+
+ async deleteNotes (data) {
+    try {
+        await writeFileAsync(noteData, JSON.stringify(data, null, '\t')).then(() => {
+            console.log('Note deleted.');
+
+        }
+        );
+    
+ } catch (error) {
+    throw error;
+ }
+ }}
+ module.exports = new DB();
